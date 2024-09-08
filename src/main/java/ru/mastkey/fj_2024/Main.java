19dbdac802e5693dec15869f2e60fdadfaf56d68
mastkey <ilya.fetyukhin@mail.ru>
2024-09-08 11:24:49 +0300

    added CustomLinkedList

--- src/main/java/ru/mastkey/fj_2024/Main.java
+++ src/main/java/ru/mastkey/fj_2024/Main.java
@@ -2,44 +2,52 @@ package ru.mastkey.fj_2024;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import ru.mastkey.fj_2024.model.City;
-import ru.mastkey.fj_2024.parser.JsonParser;
-import ru.mastkey.fj_2024.parser.XmlParser;
+import ru.mastkey.fj_2024.list.CustomLinkedList;
 
-import java.nio.file.Path;
+import java.util.stream.IntStream;
 
 public class Main {
+
     private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
-    private static final String MSG_WARNING_WITH_JSON = "Exception while parsing json";
-    private static final String MSG_PARSED_OBJECT = "Parsed object: %s";
-    private static final String MSG_PARSED_XML = "Parsed object into xml path: %s";
-    private static final String MSG_WARNING_WITH_XML = "Exception while parsing xml";
 
     public static void main(String[] args) {
-        var notValidResource = Path.of(ClassLoader.getSystemResource("city-error.json").getPath());
-        var parsedNotValidObject = JsonParser.parseFromJson(notValidResource, City.class);
+        var customLinkedList = new CustomLinkedList<Integer>();
 
-        if (parsedNotValidObject.isPresent()) {
-            LOGGER.info(String.format(MSG_PARSED_OBJECT, parsedNotValidObject.get()));
-        } else {
-            LOGGER.warn(MSG_WARNING_WITH_JSON);
-        }
+        customLinkedList.add(1);
+        LOGGER.info("After added 1: " + customLinkedList);
 
-        var validResource = Path.of(ClassLoader.getSystemResource("city.json").getPath());
-        var parsedValidObject = JsonParser.parseFromJson(validResource, City.class);
+        var element = customLinkedList.get(0);
+        LOGGER.info("Element on 0 index: " + element.toString());
 
-        if (parsedValidObject.isPresent()) {
-            LOGGER.info(String.format(MSG_PARSED_OBJECT, parsedValidObject.get()));
-        } else {
-            LOGGER.warn(MSG_WARNING_WITH_JSON);
-        }
+        var size = customLinkedList.size();
+        LOGGER.info("Size of list: " + size);
 
-        var parsedIntoXmlCityPath = XmlParser.parseToXml(parsedValidObject.get(), "city");
+        var contains = customLinkedList.contains(1);
+        LOGGER.info("Contains element: " + contains);
 
-        if (parsedIntoXmlCityPath.isPresent()) {
-            LOGGER.info(String.format(MSG_PARSED_XML, parsedIntoXmlCityPath.get()));
-        } else {
-            LOGGER.warn(MSG_WARNING_WITH_XML);
-        }
+        customLinkedList.remove(0);
+        LOGGER.info("After removed 1: " + customLinkedList);
+
+        var list = IntStream.range(0, 10)
+                .boxed()
+                .toList();
+
+        customLinkedList.addAll(list);
+        LOGGER.info("After added 10 elements from list: " + customLinkedList);
+
+        CustomLinkedList<Integer> cstm = IntStream.range(0, 15)
+                .boxed()
+                .reduce(new CustomLinkedList<>(),
+                        (listt, el) -> {
+                            listt.add(el);
+                            return listt;
+                        },
+                        (list1, list2) -> {
+                            list1.addAll(list2);
+                            return list1;
+                        }
+                );
+
+        LOGGER.info("After added 15 elements from stream: " + cstm);
     }
 }
---
+++ src/main/java/ru/mastkey/fj_2024/list/CustomLinkedList.java
@@ -0,0 +1,153 @@
+package ru.mastkey.fj_2024.list;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class CustomLinkedList<E> implements Iterable<E> {
+    private final Node<E> head;
+    private final Node<E> tail;
+    private int size;
+
+    public CustomLinkedList() {
+        this.head = new Node<>(null, null, null);
+        this.tail = new Node<>(null, null, null);
+        this.size = 0;
+        head.next = tail;
+        tail.prev = head;
+    }
+
+    public void add(E e) {
+        var prev = tail.prev;
+        var node = new Node<>(prev, e, this.tail);
+        prev.next = node;
+        tail.prev = node;
+        size++;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void remove(int index) {
+        var nodeForDelete = getNodeByIndex(index);
+        nodeForDelete.prev.next = nodeForDelete.next;
+        nodeForDelete.next.prev = nodeForDelete.prev;
+        size--;
+    }
+
+    public boolean contains(E e) {
+        var temp = head.next;
+        if (e == null) {
+            while (temp != tail) {
+                if (temp.item == e) {
+                    return true;
+                }
+                temp = temp.next;
+            }
+        } else {
+            while (temp != tail) {
+                if (e.equals(temp.item)) {
+                    return true;
+                }
+                temp = temp.next;
+            }
+        }
+        return false;
+    }
+
+    public E get(int index) {
+        return getNodeByIndex(index).item;
+    }
+
+    public void addAll(Collection<? extends E> collection) {
+        for (E e : collection) {
+            add(e);
+        }
+    }
+
+    public void addAll(CustomLinkedList<? extends E> collection) {
+        for (E e : collection) {
+            add(e);
+        }
+    }
+
+    private Node<E> getNodeByIndex(int index) {
+        validateIndex(index);
+
+        Node<E> temp;
+        if (index > size / 2) {
+            temp = tail;
+            for (int i = size; i > index; i--) {
+                temp = temp.prev;
+            }
+        } else {
+            temp = head.next;
+            for (int i = 0; i < index; i++) {
+                temp = temp.next;
+            }
+        }
+        return temp;
+    }
+
+    private void validateIndex(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return new CustomLinkedListIterator();
+    }
+
+    private class CustomLinkedListIterator implements Iterator<E> {
+        private Node<E> current = head.next;
+
+        @Override
+        public boolean hasNext() {
+            return current != tail;
+        }
+
+        @Override
+        public E next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            E item = current.item;
+            current = current.next;
+            return item;
+        }
+
+        @Override
+        public void remove() {
+            throw new UnsupportedOperationException("Remove not supported");
+        }
+    }
+
+    private static class Node<E> {
+        final E item;
+        Node<E> next;
+        Node<E> prev;
+
+        Node(Node<E> prev, E element, Node<E> next) {
+            this.item = element;
+            this.next = next;
+            this.prev = prev;
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+        for (Node<E> node = head.next; node != tail; node = node.next) {
+            sb.append(node.item);
+            if (node.next != tail) {
+                sb.append(", ");
+            }
+        }
+        sb.append("]");
+        return sb.toString();
+    }
+}
