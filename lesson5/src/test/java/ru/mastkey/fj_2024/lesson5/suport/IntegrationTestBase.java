a7f4f4652caaf8ac38bcc9a2fa7e346b08790fc8
mastkey <ilya.fetyukhin@mail.ru>
2024-11-09 11:18:58 +0300

    wip

--- lesson5/src/test/java/ru/mastkey/fj_2024/lesson5/suport/IntegrationTestBase.java
+++ lesson5/src/test/java/ru/mastkey/fj_2024/lesson5/suport/IntegrationTestBase.java
@@ -1,7 +1,11 @@
 package ru.mastkey.fj_2024.lesson5.suport;
 
+import com.github.tomakehurst.wiremock.WireMockServer;
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
 import org.jeasy.random.EasyRandom;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,9 +14,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.reactive.server.WebTestClient;
-import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
-import org.wiremock.integrations.testcontainers.WireMockContainer;
 import ru.mastkey.fj_2024.lesson5.entity.Category;
 import ru.mastkey.fj_2024.lesson5.entity.Event;
 import ru.mastkey.fj_2024.lesson5.entity.Location;
@@ -21,7 +23,6 @@ import ru.mastkey.fj_2024.lesson5.repository.EntityRepository;
 import ru.mastkey.fj_2024.lesson5.repository.EventRepository;
 import ru.mastkey.fj_2024.lesson5.repository.PlaceRepository;
 
-import java.io.File;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
@@ -29,27 +30,10 @@ import java.util.UUID;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
 @Testcontainers
-@ContextConfiguration(initializers = PostgreSQLInitializer.class)
+@ContextConfiguration(initializers = {PostgreSQLInitializer.class, WiremockInitializer.class})
 public class IntegrationTestBase {
 
     protected static final EasyRandom easyRandom = new EasyRandom();
-
-    @Container
-    private static final WireMockContainer wiremockServer = new WireMockContainer("wiremock/wiremock:2.35.0")
-            .withMappingFromResource("wiremock/categories.json")
-            .withMappingFromResource("wiremock/locations.json")
-            .withFile(new File("src/test/resources/__files/currency.xml"))
-            .withMappingFromResource("wiremock/currency.json");
-
-    @DynamicPropertySource
-    static void overrideKudaGoProperties(DynamicPropertyRegistry registry) {
-        String wireMockUrl = String.format("http://%s:%d",
-                wiremockServer.getHost(),
-                wiremockServer.getMappedPort(8080));
-        registry.add("kudago.category-url", () -> wireMockUrl + "/public-api/v1.4/place-categories");
-        registry.add("kudago.location-url", () -> wireMockUrl + "/public-api/v1.4/locations");
-        registry.add("cb.currency-url", () -> wireMockUrl + "/scripts/XML_daily.asp");
-    }
 
     @Autowired
     protected WebTestClient webClient;
